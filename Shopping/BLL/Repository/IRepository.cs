a505f26d83c055ad640d56fb7059e78c03f40beb
BurakOZN <burakozn@gmail.com>
2019-07-16 11:08:22 +0300

    Crud işlemleri için Repository inteface ve classi yazıldı.

---
+++ Shopping/BLL/Repository/IRepository.cs
@@ -0,0 +1,31 @@
+﻿using Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace BLL.Repository
+{
+    public interface IRepository<T> where T:BaseEntity
+    {
+        CrudState Add(T entity);
+        CrudState Add(IEnumerable<T> entity);
+        List<T> Get();
+        List<T> Get(Expression<Func<T, bool>> where);
+        CrudState Update(T entity);
+        CrudState Delete(string id);
+        CrudState Delete(IEnumerable<T> entities);
+    }
+    public enum CrudState
+    {
+        Success,
+        EntityError,
+        ConnectionError,
+        NotFound
+    }
+    public enum ConnectionState
+    {
+        Success,
+        ConnectionError
+    }
+}
---
+++ Shopping/BLL/Repository/Repository.cs
@@ -0,0 +1,133 @@
+﻿using DAL;
+using Entity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace BLL.Repository
+{
+    /***
+     * Savechanges işlemini toplu yapmak için Unit of Work veya save metodu yazılıyor ancak burada addrange ce updaterange metodları olduğu için görek görmedim
+     * update metodu state ile yapılacak güncelleme gelecek.
+     * */
+    public class Repository<T> : IRepository<T> where T : BaseEntity
+    {
+        public ShoppingContext _db;
+        private readonly DbSet<T> entitySet;
+        public Repository(ShoppingContext db)
+        {
+            _db = db;
+            entitySet = _db.Set<T>();
+        }
+
+        public CrudState Add(T entity)
+        {
+            try
+            {
+                entity.Id = Guid.NewGuid().ToString();
+                entity.CreateAt = DateTime.Now;
+                entity.UpdateAt = DateTime.Now;
+                entitySet.Add(entity);
+                return Save() == ConnectionState.Success ? CrudState.Success : CrudState.ConnectionError;
+            }
+            catch
+            {
+                return CrudState.EntityError;
+            }
+        }
+
+        public CrudState Add(IEnumerable<T> entities)
+        {
+            try
+            {
+                foreach (var item in entities)
+                {
+                    item.Id = Guid.NewGuid().ToString();
+                }
+                entitySet.AddRange(entities);
+                return Save() == ConnectionState.Success ? CrudState.Success : CrudState.ConnectionError;
+            }
+            catch
+            {
+                return CrudState.ConnectionError;
+            }
+        }
+
+        public CrudState Delete(string id)
+        {
+            try
+            {
+                var entity = entitySet.Find(id);
+                entitySet.Remove(entity);
+                return Save() == ConnectionState.Success ? CrudState.Success : CrudState.ConnectionError;
+            }
+            catch (Exception)
+            {
+                return CrudState.EntityError;
+            }
+        }
+
+        public CrudState Delete(IEnumerable<T> entities)
+        {
+            try
+            {
+                entitySet.RemoveRange(entities);
+                return Save() == ConnectionState.Success ? CrudState.Success : CrudState.ConnectionError;
+            }
+            catch (Exception)
+            {
+                return CrudState.EntityError;
+            }
+        }
+
+        public List<T> Get()
+        {
+            return _db.Set<T>().ToList();
+        }
+
+        public List<T> Get(Expression<Func<T, bool>> where)
+        {
+            return _db.Set<T>().Where(where).ToList();
+        }
+
+        public CrudState Update(T entity)
+        {
+            T old = _db.Set<T>().Find(entity.Id);
+            if (old != null)
+            {
+                try
+                {
+                    entity.UpdateAt = DateTime.Now;
+                    _db.Entry(old).CurrentValues.SetValues(entity);
+                    return Save() == ConnectionState.Success ? CrudState.Success : CrudState.ConnectionError;
+                }
+                catch (Exception)
+                {
+
+                    return CrudState.EntityError;
+                }
+            }
+            else
+                return CrudState.NotFound;
+        }
+
+
+
+        private ConnectionState Save()
+        {
+            try
+            {
+                _db.SaveChanges();
+                return ConnectionState.Success;
+            }
+            catch (Exception ex)
+            {
+                return ConnectionState.ConnectionError;
+            }
+        }
+    }
+
+}
