01be593cde45409fc8256781daa41dc5b473a691
BurakOZN <burakozn@gmail.com>
2019-07-16 17:03:13 +0300

    Teslimat metodları yazıldı.

---
+++ Shopping/BLL/Manager/DelivertCostCalculator.cs
@@ -0,0 +1,28 @@
+﻿using Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BLL.Manager
+{
+    public class DeliveryCostCalculator
+    {
+        private readonly decimal _costPerDelivery;
+        private readonly decimal _costPerProduct;
+        private readonly decimal _fixedCost;
+        public DeliveryCostCalculator(decimal costPerDelivery, decimal costPerProduct, decimal fixedCost)
+        {
+            _costPerDelivery = costPerDelivery;
+            _costPerProduct = costPerProduct;
+            _fixedCost = fixedCost;
+
+        }
+        public double calculateFor(Cart cart)
+        {
+            var NumberOfDeliveries = cart.Products.GroupBy(x => x.Product.CategoryId).Count();
+            var NumberOfProducts = cart.Products.GroupBy(x => x.Product.Id).Count();
+            return (double)(_costPerDelivery * NumberOfDeliveries+_costPerProduct* NumberOfProducts+_fixedCost);
+        }
+    }
+}
---
+++ Shopping/BLL_Tests/DeliveryCostCalculatorTests.cs
@@ -0,0 +1,37 @@
+﻿using Entity;
+using NUnit.Framework;
+using BLL.Manager;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL_Tests
+{
+    [TestFixture]
+    public class DeliveryCostCalculatorTests
+    {
+        [Test]
+        public void DeliveryCostCalculator_CalculateFor3Product_Returns()
+        {
+            var category = new Category("food");
+            category.SubCategory = new List<Category>() { new Category("Sub1"), new Category("Sub2") };
+
+            var product = new Product("Apple", 100.0m, category.SubCategory[0]);
+            var product3 = new Product("Banana", 50.0m, category.SubCategory[0]);
+            var product2 = new Product("Almonds", 150.0m, category.SubCategory[1]);
+            var cart = new Cart("TestCart");
+
+            cart.AddProduct(product, 3);
+            cart.AddProduct(product3, 2);
+            cart.AddProduct(product2, 5);
+
+            DeliveryCostCalculator calculator = new DeliveryCostCalculator(1, 2, 3);
+            var data = calculator.calculateFor(cart);
+
+            Assert.That(data, Is.EqualTo(11));
+
+        }
+    }
+}
